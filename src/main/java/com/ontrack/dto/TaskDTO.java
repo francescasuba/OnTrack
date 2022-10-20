44b34f84f9be335c3f11046d2be4fe9d174ee5bf
Francesca Suba <72313800+francescasuba@users.noreply.github.com>
2022-10-20 14:51:44 -0400

    Getters and setters in taskDTO

--- src/main/java/com/ontrack/dto/TaskDTO.java
+++ src/main/java/com/ontrack/dto/TaskDTO.java
@@ -3,6 +3,30 @@ package com.ontrack.dto;
 public class TaskDTO {
 
 	private int taskId;
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getTaskType() {
+		return taskType;
+	}
+
+	public void setTaskType(String taskType) {
+		this.taskType = taskType;
+	}
+
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+
 	private String taskType;
 	private String taskDescription;
 	
