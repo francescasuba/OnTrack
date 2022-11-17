85d73c9041a32dad73573dd910db0f6748ceb184
Francesca Suba <72313800+francescasuba@users.noreply.github.com>
2022-11-16 21:03:28 -0500

    WIP sorry it's not good

--- src/main/java/com/ontrack/OnTrackController.java
+++ src/main/java/com/ontrack/OnTrackController.java
@@ -1,16 +1,14 @@
 package com.ontrack;
 
 import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ontrack.dto.TaskDTO;
 import com.ontrack.service.ITaskService;
-import com.soit.soitfaculty.entity.Faculty;
 
 @Controller
 @RequestMapping("/Tasks")
